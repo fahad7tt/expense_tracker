b65402e0cb6bee636c0b2f2ab514db09a47d59da
Fahad TT <ttfahad2317@gmail.com>
2024-07-21 22:11:32 +0530

    bottom navbar modified

--- lib/presentation/widgets/bottom_navbar/bottom_navbar.dart
+++ lib/presentation/widgets/bottom_navbar/bottom_navbar.dart
@@ -13,64 +13,90 @@ class BottomNavBar extends StatelessWidget {
       builder: (context, navProvider, child) {
         return Stack(
           clipBehavior: Clip.none,
-          children:[
-           BottomAppBar(
-            color: navbarColor,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: const Icon(
-                  Icons.home_rounded,
-                  color: darkColor,
-                  size: iconSize),
-                  onPressed: () {
-                    navProvider.setIndex(0);
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(
-                  Icons.summarize_rounded,
-                  color: darkColor,
-                  size: iconSize),
-                  onPressed: () {
-                    navProvider.setIndex(1);
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.person,
-                  color: darkColor,
-                  size: iconSize),
-                  onPressed: () {
-                    navProvider.setIndex(2);
-                  },
-                ),
-                const SizedBox(width: 48), // Space for the FAB
-              ],
+          children: [
+            BottomAppBar(
+              color: navbarColor,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  _buildNavItem(
+                    context,
+                    icon: Icons.home_filled,
+                    label: 'Home',
+                    index: 0,
+                    navProvider: navProvider,
+                  ),
+                  _buildNavItem(
+                    context,
+                    icon: Icons.summarize_rounded,
+                    label: 'Summary',
+                    index: 1,
+                    navProvider: navProvider,
+                  ),
+                 
+                  _buildNavItem(
+                    context,
+                    icon: Icons.person_rounded,
+                    label: 'Profile',
+                    index: 2,
+                    navProvider: navProvider,
+                  ),
+                   const SizedBox(width: 64), // Placeholder for the FAB
+                ],
+              ),
             ),
-                     ),
-          Positioned(
-          bottom: 12,
-          right: 16,
-          child: Transform.scale(
-            scale: 1.0, // Scale factor for the FAB size 
-            child: FloatingActionButton(
-              onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => AddExpensePage(),
-                    ),
-                  );
-                },
-              splashColor: navbarColor,
-              tooltip: 'Add Expense',
-              child: const Icon(Icons.add),
+            Positioned(
+              bottom: 12,
+              right: 20,
+              child: Transform.scale(
+                scale: 1.0, // Scale factor for the FAB size 
+                child: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => AddExpensePage(),
+                      ),
+                    );
+                  },
+                  splashColor: navbarColor,
+                  tooltip: 'Add Expense',
+                  child: const Icon(Icons.add),
+                ),
+              ),
             ),
-          ),
-        ),
-      ],
+          ],
         );
       },
+    );
+  }
+
+  Widget _buildNavItem(BuildContext context, {
+    required IconData icon,
+    required String label,
+    required int index,
+    required NavigationProvider navProvider,
+  }) {
+    return GestureDetector(
+      onTap: () {
+        navProvider.setIndex(index);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              color: darkColor,
+              size: iconSize,
+            ),
+            Text(
+              label,
+              style: const TextStyle(color: deepBlue, fontSize: 11),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
