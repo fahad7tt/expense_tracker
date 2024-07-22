b319ae355bfdb8182bef14ef0c3897cc355613a1
Fahad TT <ttfahad2317@gmail.com>
2024-07-22 20:51:38 +0530

    feat: terms and conditions added

---
+++ lib/presentation/pages/profile/terms_and_conditions/terms_and_conditions.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:personal_expense_tracker/presentation/pages/profile/profile_page.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class TermsAndConditions extends StatefulWidget {
+  const TermsAndConditions({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TermsState createState() => _TermsState();
+}
+
+class _TermsState extends State<TermsAndConditions> {
+  late WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            const CircularProgressIndicator();
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://www.freeprivacypolicy.com/live/6cc831e2-3af6-4eca-b699-45fc33779e86'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Terms and Conditions'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ProfilePage()),
+            );
+          },
+        ),
+      ),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
