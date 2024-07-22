631b6f7a38a73916daf5f8fec01957af4e62eee1
Fahad TT <ttfahad2317@gmail.com>
2024-07-22 21:08:47 +0530

    refactor: routes updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expense_tracker/core/utils/theme/theme_data.dart';
 import 'package:personal_expense_tracker/presentation/pages/expense_list/expense_list_page.dart';
+import 'package:personal_expense_tracker/presentation/pages/profile/app_info/app_info.dart';
+import 'package:personal_expense_tracker/presentation/pages/profile/privacy_policy/privacy_policy.dart';
+import 'package:personal_expense_tracker/presentation/pages/profile/terms_and_conditions/terms_and_conditions.dart';
 import 'package:personal_expense_tracker/presentation/pages/splash_screen/splash_screen.dart';
 import 'package:personal_expense_tracker/presentation/pages/expense_summary/expense_summary_pages.dart';
 import 'package:personal_expense_tracker/presentation/pages/profile/profile_page.dart';
@@ -35,9 +38,12 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         routes: {
           '/': (context) => const SplashScreen(),
-          '/home':  (context) => ExpenseListPage(),
+          '/home': (context) => ExpenseListPage(),
           '/summary': (context) => const ExpenseSummaryPage(),
           '/profile': (context) => const ProfilePage(),
+          '/appInfo': (context) => const AppInfo(),
+          'termsAndConditions': (context) => const TermsAndConditions(),
+          'privacyPolicy': (context) => const PrivacyPolicy()
         },
       ),
     );
