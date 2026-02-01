a1fbfeab27712bfaa114e618f9f717872f0bf4d2
Fahad TT <ttfahad2317@gmail.com>
2026-02-01 14:42:41 +0530

    removed the initially created splash screen, default initial route is now ExpenseListPage

--- lib/core/utils/routes/routes.dart
+++ lib/core/utils/routes/routes.dart
@@ -3,13 +3,12 @@ import 'package:personal_expense_tracker/presentation/pages/expense_list/expense
 import 'package:personal_expense_tracker/presentation/pages/profile/app_info/app_info.dart';
 import 'package:personal_expense_tracker/presentation/pages/profile/privacy_policy/privacy_policy.dart';
 import 'package:personal_expense_tracker/presentation/pages/profile/terms_and_conditions/terms_and_conditions.dart';
-import 'package:personal_expense_tracker/presentation/pages/splash_screen/splash_screen.dart';
 import 'package:personal_expense_tracker/presentation/pages/expense_summary/expense_summary_pages.dart';
 import 'package:personal_expense_tracker/presentation/pages/profile/profile_page.dart';
 
 Map<String, WidgetBuilder> get appRoutes {
   return {
-    '/': (context) => const SplashScreen(),
+    '/': (context) => ExpenseListPage(),
     '/home': (context) => ExpenseListPage(),
     '/summary': (context) => const ExpenseSummaryPage(),
     '/profile': (context) => const ProfilePage(),
