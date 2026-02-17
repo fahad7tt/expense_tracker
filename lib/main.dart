8f87b923a3535aa0afa6d839d79e2891a1995767
Fahad TT <ttfahad2317@gmail.com>
2026-02-17 16:56:55 +0530

    fix: delete button issue and privacy policy issues in release mode

--- lib/main.dart
+++ lib/main.dart
@@ -6,5 +6,5 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
 
-  runApp(const MyApp());
+  runApp(const MyApp()); 
 }
--- lib/presentation/widgets/confirm_dialog/confirm_dialog.dart
+++ lib/presentation/widgets/confirm_dialog/confirm_dialog.dart
@@ -4,42 +4,66 @@ import 'package:personal_expense_tracker/core/utils/theme/system_theme.dart';
 
 class DialogService {
   static Future<void> showDeleteConfirmationDialog(
-      BuildContext context, VoidCallback onConfirmed) async {
+    BuildContext context,
+    VoidCallback onConfirmed,
+  ) async {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           backgroundColor: context.isDarkMode ? lightColor : deepBlue,
-          title: Text('Delete Expense',
-              style: TextStyle(
-                  color: context.isDarkMode ? errorColor : lightColor,
-                  fontWeight: FontWeight.w500)),
-          content: Text('Are you sure you want to delete?',
-              style: TextStyle(
-                  color: context.isDarkMode ? darkColor : lightColor,
-                  fontWeight: FontWeight.w600)),
-          actions: <Widget>[
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: context.isDarkMode ? darkColor : lightColor,
-                foregroundColor: context.isDarkMode ? lightColor : deepBlue,
-              ),
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+          title: Text(
+            'Delete Expense',
+            style: TextStyle(
+              color: context.isDarkMode ? errorColor : lightColor,
+              fontWeight: FontWeight.w500,
             ),
-            const Spacer(),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: context.isDarkMode ? errorColor : lightColor,
-                foregroundColor: context.isDarkMode ? lightColor : errorColor,
-              ),
-              child: const Text('Delete'),
-              onPressed: () {
-                onConfirmed();
-                Navigator.of(context).pop();
-              },
+          ),
+          content: Text(
+            'Are you sure you want to delete?',
+            style: TextStyle(
+              color: context.isDarkMode ? darkColor : lightColor,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          actions: <Widget>[
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: context.isDarkMode
+                          ? darkColor
+                          : lightColor,
+                      foregroundColor: context.isDarkMode
+                          ? lightColor
+                          : deepBlue,
+                    ),
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: context.isDarkMode
+                          ? errorColor
+                          : lightColor,
+                      foregroundColor: context.isDarkMode
+                          ? lightColor
+                          : errorColor,
+                    ),
+                    child: const Text('Delete'),
+                    onPressed: () {
+                      onConfirmed();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              ],
             ),
           ],
         );
